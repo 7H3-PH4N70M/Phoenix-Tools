a8b6aad2c941ab8ea0824c994a69fdb4f08679e8
7H3-PH4N70M <7h3ph4n70m@gmail.com>
2017-01-13 11:50:10 -0600

    Update MAC-Spoofer.sh

--- Kali-Linux-2016.2/Staying-Anonymous/MAC-Spoofer/MAC-Spoofer.sh
+++ Kali-Linux-2016.2/Staying-Anonymous/MAC-Spoofer/MAC-Spoofer.sh
@@ -4,15 +4,17 @@ read COMMAND
 if [ COMMAND="What is my card name" ]; then
   echo "If your using wireless, it will be wlanX, if your using ethernet, it will be ethX where X is a number"
   ifconfig
+  echo "Please enter your card name"
+  read CARD_NAME
 else
   CARD_NAME=COMMAND
 fi
 
-ifconfig CARD_NAME down
+ifconfig $CARD_NAME down
 echo "Please enter your new Media Access Address"
 echo "A MAC Address loks like this: XX:XX:XX:XX:XX:XX but the Xs are replaced with number and/or letters"
 read NEW_MAC
-ifconfig CARDNAME hw ether NEW_MAC
-ifconfig CARD_NAME up
+ifconfig $CARDNAME hw ether $NEW_MAC
+ifconfig $CARD_NAME up
 clear
 echo "Your MAC Address has changed."
